21b685d448e00b4a7c06ce4cf2d290c838d86ae2
Tuan Minh Tran <trantuan23082003@gmail.com>
2023-06-15 08:56:17 +0700

    update volume.java in storage package

---
+++ sysInfo/src/resource/storage/Volume.java
@@ -0,0 +1,52 @@
+package resource.storage;
+
+import oshi.software.os.OSFileStore;
+
+public class Volume {
+    private OSFileStore vol;
+    private String name;
+    private String fileSys;
+    private long spaceTotal;
+    private long spaceAvailable;
+    private String uuid;
+
+    public Volume(OSFileStore o){
+        this.vol = o;
+        this.name = o.getLabel();
+        this.fileSys = o.getName();
+        this.spaceTotal = o.getTotalSpace();
+        this.spaceAvailable = o.getFreeSpace(); // or getUsableSpace?
+        this.uuid = o.getUUID();
+        }
+    
+    public void updateSpace(){
+        this.spaceTotal =  vol.getTotalSpace();
+        this.spaceAvailable = vol.getFreeSpace(); // rem to fix
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    //public String getFilesys() {
+    //    return fileSys;
+    //}
+
+    public String getfileSys(){
+        return fileSys;
+    }
+
+    public long getSpaceTotal() {
+        return spaceTotal;
+    }
+
+    public long getSpaceAvailable() {
+        return spaceAvailable;
+    }
+
+    public String getUUID() {
+        return uuid;
+    }
+}
